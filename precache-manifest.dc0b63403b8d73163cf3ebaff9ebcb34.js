04973e5bed6c6703bb5a8ffd2ad38d102db96b19
darwincosmos <dar.cosmos97@gmail.com>
2020-08-20 15:49:29 -0500

    Updates

---
+++ precache-manifest.dc0b63403b8d73163cf3ebaff9ebcb34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bdf9cf3bd9bc68eea98865653c685c0",
+    "url": "/elrincondelvago/index.html"
+  },
+  {
+    "revision": "d485228772383588c0bf",
+    "url": "/elrincondelvago/static/css/main.6ea04d8d.chunk.css"
+  },
+  {
+    "revision": "9118fb2a13b39277b2be",
+    "url": "/elrincondelvago/static/js/2.aa5044b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/elrincondelvago/static/js/2.aa5044b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d485228772383588c0bf",
+    "url": "/elrincondelvago/static/js/main.df7ef375.chunk.js"
+  },
+  {
+    "revision": "513ef830690d8b6cd20d",
+    "url": "/elrincondelvago/static/js/runtime-main.9d37f07f.js"
+  }
+]);
